4cd071f0ceb2d73811eb9bfca409dfeef56af8f7
Dhaval Desai <76671740+dhavald23@users.noreply.github.com>
2022-09-24 19:05:33 -0700

    Add files via upload

---
+++ image-to-sound.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+from PIL import Image
+from gtts import gTTS
+from pytesseract import image_to_string
+
+
+def image_to_sound(path_to_image):
+    """
+    Function for converting an  image to sound
+    """
+    try:
+        loaded_image = Image.open(path_to_image)
+        decoded_text = image_to_string(loaded_image)
+        cleaned_text = " ".join(decoded_text.split("\n"))
+        print(cleaned_text)
+        sound = gTTS(cleaned_text, lang="en")
+        sound.save("sound.mp3")
+        return True
+    except Exception as bug:
+        print("The bug thrown while excuting the code\n", bug)
+        return
+
+
+if __name__ == "__main__":
+    image_to_sound("./PycharmProjects/pythonProject2/image.jpg")
+    input()
+
+
+# In[ ]:
+
+
+
+
